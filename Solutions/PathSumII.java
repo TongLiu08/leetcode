a7c2a8ebc32fcc74ff204388e4ba4cf0f7c039ca
TongLiu08 <lmzltlmzlt01@gmail.com>
2013-10-31 21:24:50 -0400

    change path sum II

--- Solutions/PathSumII.java
+++ Solutions/PathSumII.java
@@ -8,29 +8,27 @@
  * }
  */
 public class Solution {
-    public ArrayList<ArrayList<Integer>> result;
     public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         
-        
-        helper(root, sum, new ArrayList<Integer>());
+        helper(root, sum, new ArrayList<Integer>(), result);
         return result;
     }
     
-    public void helper(TreeNode node, int sum, ArrayList<Integer> partialPath) {
+    public void helper(TreeNode node, int sum, ArrayList<Integer> partialPath, ArrayList<ArrayList<Integer>> result) {
         if (node == null)
             return;
             
         partialPath.add(node.val);
-        ArrayList<Integer> temp1 = new ArrayList<Integer>(partialPath);
-        ArrayList<Integer> temp2 = new ArrayList<Integer>(partialPath);
         if (node.left == null && node.right == null && node.val == sum) {
-            result.add(partialPath);
+            ArrayList<Integer> temp = new ArrayList<Integer>(partialPath);
+            result.add(temp);
         }
         
-        helper(node.left, sum - node.val, temp1);
-        helper(node.right, sum - node.val, temp2);
+        helper(node.left, sum - node.val, partialPath, result);
+        helper(node.right, sum - node.val, partialPath, result);
+        partialPath.remove(partialPath.size() - 1);
     }
 }
